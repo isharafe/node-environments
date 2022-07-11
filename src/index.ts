b7dd31a262d075a6e17d5c0cd86e790f90777860
Ruchira <ruchira@coredevone.com>
2022-07-11 18:23:38 +0530

    use environment specific values in separate env files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
+const environment = process.env.NODE_ENV;
+dotenv.config({ path: `./env/${environment}.env`});
 
 const app = express();
 
 app.get("/", (req: Request, res: Response)=> {
     const envValue = process.env.myValue;
-    const environment = "";
     return res.send(`environment: ${environment}. env value: ${envValue}`);
 });
 
